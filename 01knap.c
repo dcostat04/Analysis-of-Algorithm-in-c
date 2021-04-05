e7ba8f445db2d8d151848750f4db4d5ddcdbdc1e
Trevor Aaron Robert Dcosta <63400592+dcostat04@users.noreply.github.com>
2021-04-05 17:06:04 +0530

    Update 01knap.c

--- 01knap.c
+++ 01knap.c
@@ -1,53 +1,80 @@
-#include <stdio.h>
-int max(int a, int b)
+#include<stdio.h>
+#include<conio.h>
+#define MAX 30
+int maximum(int a, int b);
+void zoknapsack(int n, int capacity);
+int profits[MAX], weights[MAX];
+int includeditems[MAX];
+int main()
 {
-    return (a > b) ? a : b;
+    int i,j,n,capacity;
+    printf("Enter the number of items : ");
+    scanf("%d",&n);
+    printf("Enter the profits and weights of each item\n");
+    for (i=0;i<n;++i)
+    {
+        printf("Item %d : ",i+1);
+        scanf("%d %d",&profits[i],&weights[i]);
+    }
+    printf("Enter the capacity of knapsack : ");
+    scanf("%d",&capacity);
+    zoknapsack(n,capacity);
+    return 0;
 }
 
-int knapSack(int W, int wt[], int val[], int n)
+void zoknapsack(int n, int capacity)
 {
-    int i, w;
-    int K[n + 1][W + 1];
-
-    printf("Table \n");
-    for (i = 0; i <= n; i++)
+    int t[n+1][capacity+1],maxprofit=0;
+    int i,j,k=0;
+    for (i=0;i<=n;++i)
     {
-        for (w = 0; w <= W; w++)
+        for (j=0;j<=capacity;++j)
         {
-            if (i == 0 || w == 0)
-                K[i][w] = 0;
-            else if (wt[i - 1] <= w)
-                K[i][w] = max(val[i - 1]
-                          + K[i - 1][w - wt[i - 1]],
-                          K[i - 1][w]);
+            if (i==0 || j==0)
+            {
+                t[i][j]=0;
+            }
+            else if (j>=weights[i-1])
+            {
+                t[i][j]=maximum(t[i-1][j], t[i-1][j-weights[i-1]] + profits[i-1]);
+            }
             else
-                K[i][w] = K[i - 1][w];
-
-            printf("%d \t",K[i][w]);
+            {
+                t[i][j]=t[i-1][j];
+            }
+            printf("%d ",t[i][j]);
         }
         printf("\n");
     }
-
-    return K[n][W];
+    for(i=1;i<=n;i++)
+        includeditems[i]=0;
+    int c=0;
+    maxprofit=t[n][capacity];
+    printf("Maximum Profit : %d",maxprofit);
+    printf("\n");
+    for (i=n;i>0 && maxprofit>=0;--i)
+    {
+        if (maxprofit == t[i-1][capacity])
+        {
+            continue;
+        }
+        else
+        {
+            //printf("%d ",weights[i-1]);
+            maxprofit-=profits[i-1];
+            capacity-=weights[i-1];
+            includeditems[i]=1;
+            printf("Item %d ",i);
+        }
+    }
+    printf(" are the items included in the knapsack");
+    printf("\n0/1 Array: [");
+    for(i=1;i<=n;i++)
+        printf("%d ",includeditems[i]);
+    printf("]");
 }
 
-int main()
+int maximum(int a, int b)
 {
-    int i;
-    int val[4];
-    int wt[4];
-    int W;
-    int n = 4;
-    printf("Enter all profits: \n");
-    for(i=0;i<n;i++){
-        scanf("%d",&val[i]);
-    }
-    printf("Enter all weights : \n");
-    for(i=0;i<n;i++){
-        scanf("%d",&wt[i]);
-    }
-    printf("Enter Maximum capacity: \n");
-    scanf("%d",&W);
-    printf("Maximum Profit: %d \n", knapSack(W, wt, val, n));
-    return 0;
+    return (a>b?a:b);
 }
